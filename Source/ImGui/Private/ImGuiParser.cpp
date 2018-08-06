31b8bf157c322ee3cc1a01f8e33a697d9a59e1a9
n.fontes <n.fontes@student.isartdigital.com>
2018-08-06 19:03:57 +0200

    Fix parser design

--- Source/ImGui/Private/ImGuiParser.cpp
+++ Source/ImGui/Private/ImGuiParser.cpp
@@ -25,6 +25,7 @@ void UImGuiParser::ParseStruct(UStruct * Struct, void * StructValue, bool Includ
 		// Test outside the ImGuiUtils call to prevent a blank space due to the empty tab
 		if (PropertyIt)
 		{
+			bool bIsFirst = true;
 			for (; PropertyIt; ++PropertyIt)
 			{
 				UProperty* Property = *PropertyIt;
@@ -32,6 +33,14 @@ void UImGuiParser::ParseStruct(UStruct * Struct, void * StructValue, bool Includ
 
 				for (int32 Index = 0; Index < PropertyIt->ArrayDim; Index++)
 				{
+					if (!bIsFirst)
+					{
+						ImGui::Separator();
+						ImGui::Spacing();
+					}
+
+					bIsFirst = false;
+
 					ImGui::Text("%ls:", *PropertyName); ImGui::SameLine();
 
 					ImGui::Spacing();   ImGui::SameLine();
@@ -40,14 +49,126 @@ void UImGuiParser::ParseStruct(UStruct * Struct, void * StructValue, bool Includ
 					ParseProperty(Property, PropertyValue);
 
 					ImGui::Spacing();
-					if (Index < PropertyIt->ArrayDim - 1)
-					{
-						ImGui::Separator();
-						ImGui::Spacing();
-					}
 				}
 			}
 		}
+	}
+}
+
+void UImGuiParser::ParseBoolProperty(UBoolProperty * BoolProperty, void * PropertyValue)
+{
+	bool Result = BoolProperty->GetPropertyValue(PropertyValue);
+	if (ImGui::Checkbox("", &Result))
+	{
+		BoolProperty->SetPropertyValue(PropertyValue, Result);
+	}
+}
+
+void UImGuiParser::ParseArrayProperty(UArrayProperty * ArrayProperty, void * PropertyValue)
+{
+	FScriptArrayHelper ArrayHelper(ArrayProperty, PropertyValue);
+	if (ArrayHelper.Num() > 0)
+	{
+		FString CollapsingHeaderTitle = FString::Printf(TEXT("Array, Size = %d"), ArrayHelper.Num());
+		ImGui::BeginGroup();
+		if (ImGui::CollapsingHeader(TCHAR_TO_ANSI(*CollapsingHeaderTitle), ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			for (int32 i = 0; i < ArrayHelper.Num(); i++)
+			{
+				uint8* PropData = ArrayHelper.GetRawPtr(i);
+				ParseProperty(ArrayProperty->Inner, PropData);
+			}
+		}
+		ImGui::EndGroup();
+	}
+}
+
+void UImGuiParser::ParseMapProperty(UMapProperty * MapProperty, void * PropertyValue)
+{
+	FScriptMapHelper MapHelper(MapProperty, PropertyValue);
+
+	if (MapHelper.Num() > 0)
+	{
+		uint8* PropData = MapHelper.GetPairPtr(0); //Maybe prefer without check
+
+		int32 Index = 0;
+		FString CollapsingHeaderTitle = FString::Printf(TEXT("Map, Size = %d"), MapHelper.Num());
+		ImGui::BeginGroup();
+
+		if (ImGui::CollapsingHeader(TCHAR_TO_ANSI(*CollapsingHeaderTitle), ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			for (int32 Count = MapHelper.Num(); Count; PropData += MapProperty->MapLayout.SetLayout.Size, ++Index)
+			{
+				if (MapHelper.IsValidIndex(Index))
+				{
+					FString ValueString;
+					ParseProperty(MapProperty->KeyProp, PropData);
+					ImGui::SameLine();
+					ParseProperty(MapProperty->ValueProp, PropData + MapProperty->MapLayout.ValueOffset);
+
+					--Count;
+				}
+			}
+		}
+
+		ImGui::EndGroup();
+	}
+}
+
+void UImGuiParser::ParseStructProperty(UStructProperty * StructProperty, void * PropertyValue)
+{
+	if (StructProperty->Struct->GetFName() == "Color")
+	{
+		FColor * ColorPtr = (FColor *)PropertyValue;
+		FLinearColor LinearColor = ColorPtr->ReinterpretAsLinear();
+
+		if (ImGui::ColorEdit4("Color", (float*)&LinearColor))
+		{
+			FColor NewColor = LinearColor.ToFColor(false);
+
+			ColorPtr->R = NewColor.R;
+			ColorPtr->G = NewColor.G;
+			ColorPtr->B = NewColor.B;
+			ColorPtr->A = NewColor.A;
+
+			// Maybe set value, dunno
+		}
+	}
+	else if (StructProperty->Struct->GetFName() == "LinearColor")
+	{
+		FLinearColor * LinearColorPtr = (FLinearColor *)PropertyValue;
+		ImGui::ColorEdit4("LinearColor", (float*)LinearColorPtr);
+	}
+	else if (StructProperty->Struct->GetFName() == "Vector")
+	{
+		FVector * VectorPtr = (FVector *)PropertyValue;
+		// X Y and Z should be aligned
+		ImGui::InputFloat3("Vector", (float*)VectorPtr);
+	}
+	else if (StructProperty->Struct->GetFName() == "Rotator")
+	{
+		FRotator * RotatorPtr = (FRotator *)PropertyValue;
+		// Pitch Yaw and Roll should be aligned
+		ImGui::InputFloat3("Rotator", (float*)RotatorPtr);
+	}
+	else if (StructProperty->Struct->GetFName() == "Quat")
+	{
+		FQuat * QuatPtr = (FQuat *)PropertyValue;
+		// X Y Z and W should be aligned
+		ImGui::InputFloat4("Quat", (float*)QuatPtr);
+	}
+	else
+	{
+		//ImGui::Indent();
+		ImGui::BeginGroup();
+
+		if (ImGui::CollapsingHeader(TCHAR_TO_ANSI(*StructProperty->Struct->GetFName().ToString()), ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			ParseStruct(StructProperty->Struct, PropertyValue, true);
+		}
+
+		ImGui::EndGroup();
+		//ImGui::Unindent();
 	}
 }
 
@@ -55,116 +176,19 @@ void UImGuiParser::ParseProperty(UProperty * Property, void * PropertyValue)
 {
 	if (UBoolProperty * BoolProperty = Cast<UBoolProperty>(Property))
 	{
-		bool Result = BoolProperty->GetPropertyValue(PropertyValue);
-		if (ImGui::Checkbox("", &Result))
-		{
-			BoolProperty->SetPropertyValue(PropertyValue, Result);
-		}
+		ParseBoolProperty(BoolProperty, PropertyValue);
 	}
 	else if (UArrayProperty * ArrayProperty = Cast<UArrayProperty>(Property))
 	{
-		FScriptArrayHelper ArrayHelper(ArrayProperty, PropertyValue);
-		if (ArrayHelper.Num() > 0)
-		{
-			FString CollapsingHeaderTitle = FString::Printf(TEXT("Array, Size = %d"), ArrayHelper.Num());
-			ImGui::BeginGroup();
-			if (ImGui::CollapsingHeader(TCHAR_TO_ANSI(*CollapsingHeaderTitle), ImGuiTreeNodeFlags_DefaultOpen))
-			{
-				for (int32 i = 0; i < ArrayHelper.Num(); i++)
-				{
-					uint8* PropData = ArrayHelper.GetRawPtr(i);
-					ParseProperty(ArrayProperty->Inner, PropData);
-				}
-			}
-			ImGui::EndGroup();
-		}
+		ParseArrayProperty(ArrayProperty, PropertyValue);
 	}
 	else if (UMapProperty * MapProperty = Cast<UMapProperty>(Property))
 	{
-		FScriptMapHelper MapHelper(MapProperty, PropertyValue);
-
-		if (MapHelper.Num() > 0)
-		{
-			uint8* PropData = MapHelper.GetPairPtr(0); //Maybe prefer without check
-
-			int32 Index = 0;
-			FString CollapsingHeaderTitle = FString::Printf(TEXT("Map, Size = %d"), MapHelper.Num());
-			ImGui::BeginGroup();
-
-			if (ImGui::CollapsingHeader(TCHAR_TO_ANSI(*CollapsingHeaderTitle), ImGuiTreeNodeFlags_DefaultOpen))
-			{
-				for (int32 Count = MapHelper.Num(); Count; PropData += MapProperty->MapLayout.SetLayout.Size, ++Index)
-				{
-					if (MapHelper.IsValidIndex(Index))
-					{
-						FString ValueString;
-						ParseProperty(MapProperty->KeyProp, PropData);
-						ImGui::SameLine();
-						ParseProperty(MapProperty->ValueProp, PropData + MapProperty->MapLayout.ValueOffset);
-
-						--Count;
-					}
-				}
-			}
-
-			ImGui::EndGroup();
-		}
+		ParseMapProperty(MapProperty, PropertyValue);
 	}
 	else if (UStructProperty * StructProperty = Cast<UStructProperty>(Property))
 	{
-		if (StructProperty->Struct->GetFName() == "Color")
-		{
-			FColor * ColorPtr = (FColor *)PropertyValue;
-			FLinearColor LinearColor = ColorPtr->ReinterpretAsLinear();
-
-			if (ImGui::ColorEdit4("Color", (float*) &LinearColor))
-			{
-				FColor NewColor = LinearColor.ToFColor(false);
-
-				ColorPtr->R = NewColor.R;
-				ColorPtr->G = NewColor.G;
-				ColorPtr->B = NewColor.B;
-				ColorPtr->A = NewColor.A;
-
-				// Maybe set value, dunno
-			}
-		}
-		else if (StructProperty->Struct->GetFName() == "LinearColor")
-		{
-			FLinearColor * LinearColorPtr = (FLinearColor *)PropertyValue;
-			ImGui::ColorEdit4("LinearColor", (float*)LinearColorPtr);
-		}
-		else if (StructProperty->Struct->GetFName() == "Vector")
-		{
-			FVector * VectorPtr = (FVector *)PropertyValue;
-			// X Y and Z should be aligned
-			ImGui::InputFloat3("Vector", (float*)VectorPtr);
-		}
-		else if (StructProperty->Struct->GetFName() == "Rotator")
-		{
-			FRotator * RotatorPtr = (FRotator *)PropertyValue;
-			// Pitch Yaw and Roll should be aligned
-			ImGui::InputFloat3("Rotator", (float*)RotatorPtr);
-		}
-		else if (StructProperty->Struct->GetFName() == "Quat")
-		{
-			FQuat * QuatPtr = (FQuat *)PropertyValue;
-			// X Y Z and W should be aligned
-			ImGui::InputFloat4("Quat", (float*)QuatPtr);
-		}
-		else
-		{
-			//ImGui::Indent();
-			ImGui::BeginGroup();
-
-			if (ImGui::CollapsingHeader(TCHAR_TO_ANSI(*StructProperty->Struct->GetFName().ToString()), ImGuiTreeNodeFlags_DefaultOpen))
-			{
-				ParseStruct(StructProperty->Struct, PropertyValue, true);
-			}
-
-			ImGui::EndGroup();
-			//ImGui::Unindent();
-		}
+		ParseStructProperty(StructProperty, PropertyValue);
 	}
 	else
 	{
--- Source/ImGui/Public/ImGuiParser.h
+++ Source/ImGui/Public/ImGuiParser.h
@@ -10,8 +10,15 @@ class IMGUI_API UImGuiParser : public UBlueprintFunctionLibrary
 
 public:
 
+	UFUNCTION(BlueprintCallable, meta = (Category = "ImGui|Parser"))
 	static void ParseObject(UObject * Object, bool IncludeSuper = false);
-	static void ParseStruct(UStruct * Struct, void * StructValue, bool IncludeSuper = false);
-	static void ParseProperty(UProperty * Property, void * PropertyValue);
 
+	static void ParseStruct(UStruct * Struct, void * StructValue, bool IncludeSuper = false);
+
+	static void ParseBoolProperty(UBoolProperty * BoolProperty, void * PropertyValue);
+	static void ParseArrayProperty(UArrayProperty * ArrayProperty, void * PropertyValue);
+	static void ParseMapProperty(UMapProperty * MapProperty, void * PropertyValue);
+	static void ParseStructProperty(UStructProperty * StructProperty, void * PropertyValue);
+
+	static void ParseProperty(UProperty * Property, void * PropertyValue);
 };
